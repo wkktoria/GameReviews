a9f890faa9d18f15983d4d248f6e80ba4143cd8e
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-04-07 16:34:14 +0200

    Configure database connection

--- src/main/java/io/github/wkktoria/game_reviews/models/Game.java
+++ src/main/java/io/github/wkktoria/game_reviews/models/Game.java
@@ -1,12 +1,24 @@
 package io.github.wkktoria.game_reviews.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.util.Date;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Game {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
     private String developer;
     private String publisher;
--- src/main/java/io/github/wkktoria/game_reviews/models/Review.java
+++ src/main/java/io/github/wkktoria/game_reviews/models/Review.java
@@ -1,11 +1,23 @@
 package io.github.wkktoria.game_reviews.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Review {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String title;
     private String content;
     private int stars;
