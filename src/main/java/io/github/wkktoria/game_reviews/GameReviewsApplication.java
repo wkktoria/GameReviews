b48e3a5073fba70d1acca2c0241bc5d6baba4a26
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-04-07 11:06:49 +0200

    Init project

---
+++ src/main/java/io/github/wkktoria/game_reviews/GameReviewsApplication.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.game_reviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameReviewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/wkktoria/game_reviews/GameReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.game_reviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
