464608d4adc1139dfc0069b41066afd0f3b47d9f
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-04-11 17:14:23 +0200

    Create more tests for game repository

--- src/test/java/io/github/wkktoria/game_reviews/repositories/GameRepositoryTests.java
+++ src/test/java/io/github/wkktoria/game_reviews/repositories/GameRepositoryTests.java
@@ -1,6 +1,7 @@
 package io.github.wkktoria.game_reviews.repositories;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -34,5 +35,50 @@ public class GameRepositoryTests {
         // Assert
         Assertions.assertThat(savedGame).isNotNull();
         Assertions.assertThat(savedGame.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void GameRepository_FindAll_ReturnsMoreThanOneGame() {
+        // Arrange
+        Game game1 = Game.builder()
+                .name("Name")
+                .developer("Developer")
+                .publisher("Publisher")
+                .releaseDate(LocalDate.now())
+                .build();
+        Game game2 = Game.builder()
+                .name("Name")
+                .developer("Developer")
+                .publisher("Publisher")
+                .releaseDate(LocalDate.now())
+                .build();
+
+        gameRepository.save(game1);
+        gameRepository.save(game2);
+
+        // Act
+        List<Game> gameList = gameRepository.findAll();
+
+        // Assert
+        Assertions.assertThat(gameList).isNotNull();
+        Assertions.assertThat(gameList.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void GameRepository_FindById_ReturnsGame() {
+        // Arrange
+        Game game = Game.builder()
+                .name("Name")
+                .developer("Developer")
+                .publisher("Publisher")
+                .releaseDate(LocalDate.now())
+                .build();
+        gameRepository.save(game);
+
+        // Act
+        Game returnGame = gameRepository.findById(game.getId()).get();
+
+        // Assert
+        Assertions.assertThat(returnGame).isNotNull();
     }
 }
